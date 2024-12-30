3b3f96bcdc4bf2a04e73817a38e40bae577f9eb1
Anderson <anderson.dataset@gmail.com>
2024-12-30 10:13:05 -0300

    cria server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import https from 'https'
+
 let count = 0
 setInterval(()=> {
   count++
   console.log(count + ': rodando...')
 }, 1000)
+
+const server = https.createServer((req, res)=> res.end('server ok'))
+
+server.listen(3333)
